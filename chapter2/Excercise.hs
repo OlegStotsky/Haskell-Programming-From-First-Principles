43077b1d05f9612e81584c0bb4f724227a931876
Oleg Stotsky <ostotsky@gmail.com>
2019-03-13 12:43:51 +1000

    Add chapter2

---
+++ chapter2/Excercise.hs
@@ -0,0 +1,6 @@
+module Excercise where
+  letFunc = let x = 3; y = 1000
+            in x * 3 + y
+  
+  whereFunc = x * 3 + y
+              where x = 3; y = 1000
---
+++ chapter2/waxOn.hs
@@ -0,0 +1,8 @@
+waxOn = x * 5
+        where z = 7
+              y = z + 8
+              x = y ^ 2
+
+waxOff x = triple x
+
+triple x = 3 * x
